a3f731e22ea611b5aef23fbd1bc0d50c062ba5fe
exslym <76844562+exslym@users.noreply.github.com>
2023-03-14 01:08:04 +0400

    deployment to gh-pages added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+	integrations: [tailwind(), preact()],
+	site: 'https://exslym.github.io/astro_project1',
+	base: '/astro_project1',
 });
