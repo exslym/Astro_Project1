131eef69095069827fcd51f458b14800daa04127
exslym <76844562+exslym@users.noreply.github.com>
2023-03-14 01:17:50 +0400

    deployment fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), preact()],
-	site: 'https://exslym.github.io/astro_project1',
-	// base: '/astro_project1',
+	site: 'https://exslym.github.io',
 });
